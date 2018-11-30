9ffc554e1a3d20610adecf9db9a81b86576cbbf3
manjeet <manjeet.s@macrew.net>
2018-11-30 15:59:05 +0530

    create user - admin site :- UserName Shown at time of user Creation

---
+++ scripts/services/subscriptionMgmt-service.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+rEIBenniesApp.service('subscriptionMgmtService', ['$http', 'config', '$location', '$cookieStore', function ($http, config, $location, $cookieStore) {
+    this.GetAllActiveUsersByRoleId = function (payload) {
+        return $http({
+            method: 'GET', url: config.epGetAllActiveUsersByRoleId +  payload,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': sessionStorage.getItem('TT') + ' ' + sessionStorage.getItem('AT')
+            },
+            data: payload,
+        }).success(function (data, status, headers, config) {
+        }).error(function (response, status, headers, config) {
+        });
+    };
+}]);
